8de3a0445c1d62491e3df8b7883efb615f335c2f
NagaKalyani2409 <58927169+NagaKalyani2409@users.noreply.github.com>
2020-03-25 00:24:21 +0530

    Add files via upload

---
+++ S8/Model/imports.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+
+from IPython.display import Image # to display PIL images
+import matplotlib.pyplot as plt
+
+ 
